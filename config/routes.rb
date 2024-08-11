ffe8e964d0c6d6b812598144b3ea76e733eea7d8
Yaqoot-Juda <yaqootjuda@gmail.com>
2024-08-11 06:53:53 +0000

    4 was done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/index'
+  devise_for :users
 
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
